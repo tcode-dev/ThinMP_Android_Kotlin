50c3b95431dc9ca4b78cf71b25c146f4216af2a6
tkwtokyo <tkw.tokyo@gmail.com>
2021-12-29 23:51:58 +0900

    Grid

--- app/src/main/java/dev/tcode/thinmp/activity/AlbumsActivity.kt
+++ app/src/main/java/dev/tcode/thinmp/activity/AlbumsActivity.kt
@@ -3,6 +3,10 @@ package dev.tcode.thinmp.activity
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.lazy.GridCells
+import androidx.compose.foundation.lazy.LazyVerticalGrid
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
@@ -10,20 +14,28 @@ import androidx.compose.runtime.Composable
 import dev.tcode.thinmp.activity.ui.theme.ThinMPTheme
 
 class AlbumsActivity : ComponentActivity() {
+    @ExperimentalFoundationApi
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             ThinMPTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
+                    Greeting(arrayOf("Android", "ios", "web"))
                 }
             }
         }
     }
 }
 
+@ExperimentalFoundationApi
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun Greeting(names: Array<String>) {
+    LazyVerticalGrid(
+        cells = GridCells.Fixed(2)
+    ) {
+        items(names) { name ->
+            Text(text = "Hello $name!")
+        }
+    }
 }
