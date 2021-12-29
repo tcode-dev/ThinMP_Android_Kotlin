8893544cfb918c19a68e43d3054c28281bf1b699
tkwtokyo <tkw.tokyo@gmail.com>
2021-12-29 23:09:43 +0900

    add AlbumsActivity

--- app/src/main/java/dev/tcode/thinmp/constant/MainMenuEnum.kt
+++ app/src/main/java/dev/tcode/thinmp/constant/MainMenuEnum.kt
@@ -1,7 +1,9 @@
 package dev.tcode.thinmp.constant
 
+import dev.tcode.thinmp.activity.AlbumsActivity
 import dev.tcode.thinmp.activity.SongsActivity
 
 enum class MainMenuEnum(val key: String, val label: String, val link: Class<*>) {
-    SONGS("songs", "Songs", SongsActivity::class.java)
+    SONGS("songs", "Songs", SongsActivity::class.java),
+    ALBUMS("albums", "Albums", AlbumsActivity::class.java)
 }
