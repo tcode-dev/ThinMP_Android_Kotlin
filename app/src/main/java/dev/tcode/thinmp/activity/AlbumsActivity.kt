21bda2cda779ac9c1ec24ef0487294de49debb76
tkwtokyo <tkw.tokyo@gmail.com>
2021-12-29 23:35:47 +0900

    remove preview

--- app/src/main/java/dev/tcode/thinmp/activity/AlbumsActivity.kt
+++ app/src/main/java/dev/tcode/thinmp/activity/AlbumsActivity.kt
@@ -7,7 +7,6 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.tooling.preview.Preview
 import dev.tcode.thinmp.activity.ui.theme.ThinMPTheme
 
 class AlbumsActivity : ComponentActivity() {
@@ -27,12 +26,4 @@ class AlbumsActivity : ComponentActivity() {
 @Composable
 fun Greeting(name: String) {
     Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    ThinMPTheme {
-        Greeting("Android")
-    }
 }
