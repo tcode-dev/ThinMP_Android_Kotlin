545952891718cb59eb335f57b0757efbe0d4bc3b
tkwtokyo <tkw.tokyo@gmail.com>
2022-01-05 00:04:26 +0900

    remove epoxy

--- app/src/main/java/dev/tcode/thinmp/epoxy/viewHolder/MainMenuViewHolder.kt
+++
@@ -1,8 +0,0 @@
-package dev.tcode.thinmp.epoxy.viewHolder
-
-import android.widget.TextView
-import dev.tcode.thinmp.R
-
-class MainMenuViewHolder: KotlinEpoxyHolder() {
-    val primaryText by bind<TextView>(R.id.primaryText)
-}
--- app/src/main/java/dev/tcode/thinmp/epoxy/viewHolder/SongViewHolder.kt
+++
@@ -1,8 +0,0 @@
-package dev.tcode.thinmp.epoxy.viewHolder
-
-import android.widget.TextView
-import dev.tcode.thinmp.R
-
-class SongViewHolder: KotlinEpoxyHolder() {
-    val primaryText by bind<TextView>(R.id.primaryText)
-}
