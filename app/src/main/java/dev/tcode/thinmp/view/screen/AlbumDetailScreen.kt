7bcf5a6ef24ad2368b6299382d748b0b24f3e0bf
tcode <tk@tcode.dev>
2022-04-20 08:18:47 +0900

    AlbumDetailScreen  Gradient, alpha

--- app/src/main/java/dev/tcode/thinmp/view/screen/AlbumDetailScreen.kt
+++ app/src/main/java/dev/tcode/thinmp/view/screen/AlbumDetailScreen.kt
@@ -5,7 +5,6 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -13,6 +12,7 @@ import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.constraintlayout.compose.ConstraintLayout
@@ -28,11 +28,7 @@ fun AlbumDetailScreen(id: String) {
     val configuration = LocalConfiguration.current
     val screenWidth = configuration.screenWidthDp.dp
 
-    Box(
-        Modifier
-            .fillMaxWidth()
-            .background(Color.Blue)
-    ) {
+    Box(Modifier.fillMaxWidth()) {
         Column {
             ConstraintLayout(
                 Modifier
@@ -43,33 +39,36 @@ fun AlbumDetailScreen(id: String) {
 
                 viewModel.uiState.album?.getUri()?.let { ImageView(uri = it) }
                 Column(
-                    verticalArrangement = Arrangement.Center,
+                    verticalArrangement = Arrangement.Bottom,
                     modifier = Modifier
                         .fillMaxWidth()
-                        .height(50.dp)
+                        .height(200.dp)
                         .constrainAs(primary) {
-                            top.linkTo(parent.bottom, margin = (-50).dp)
+                            top.linkTo(parent.bottom, margin = (-200).dp)
                         }
                         .background(
                             brush = Brush.verticalGradient(
-                                colors = listOf(
-                                    MaterialTheme.colors.primary,
-                                    MaterialTheme.colors.primaryVariant
-                                )
+                                0.0f to Color.Black.copy(alpha = 0F),
+                                1.0F to Color.Black,
                             )
-                        )
+                        ),
                 ) {
                     viewModel.uiState.album?.name?.let {
                         Text(
                             it,
-                            Modifier.fillMaxWidth(),
+                            Modifier
+                                .fillMaxWidth()
+                                .padding(bottom = 10.dp),
+                            fontWeight = FontWeight.Bold,
                             textAlign = TextAlign.Center
                         )
                     }
                     viewModel.uiState.album?.artistName?.let {
                         Text(
                             it,
-                            Modifier.fillMaxWidth(),
+                            Modifier
+                                .fillMaxWidth()
+                                .padding(bottom = 20.dp),
                             textAlign = TextAlign.Center
                         )
                     }
