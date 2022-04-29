0edab366c52b4fc664f068517e7f20108af53315
tcode <tk@tcode.dev>
2022-04-29 12:04:33 +0900

    replaced coil

--- app/src/main/java/dev/tcode/thinmp/view/image/ImageView.kt
+++ app/src/main/java/dev/tcode/thinmp/view/image/ImageView.kt
@@ -1,41 +1,25 @@
 package dev.tcode.thinmp.view.image
 
-import android.graphics.Bitmap
-import android.graphics.ImageDecoder
 import android.net.Uri
-import androidx.compose.foundation.Image
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.asImageBitmap
 import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
+import coil.compose.AsyncImage
 import dev.tcode.thinmp.R
-import java.io.IOException
 
 @Composable
-fun ImageView(uri: Uri, modifier: Modifier = Modifier, contentScale: ContentScale = ContentScale.Fit,) {
-    var bitmap: Bitmap? = null
-    val source = ImageDecoder.createSource(LocalContext.current.contentResolver, uri)
-
-    try {
-        bitmap = ImageDecoder.decodeBitmap(source)
-    } catch (e: IOException) {
-        // handle exception.
-    }
-
-    if (bitmap != null) {
-        Image(
-            bitmap = bitmap.asImageBitmap(),
-            contentDescription = "",
-            modifier = modifier,
-            contentScale = contentScale,
-        )
-    } else {
-        Image(
-            painter = painterResource(R.drawable.song_dark),
-            contentDescription = "",
-            modifier = modifier,
-        )
-    }
+fun ImageView(
+    uri: Uri,
+    modifier: Modifier = Modifier,
+    contentScale: ContentScale = ContentScale.Fit
+) {
+    AsyncImage(
+        model = uri,
+        modifier = modifier,
+        contentScale = contentScale,
+        contentDescription = null,
+        placeholder = painterResource(R.drawable.song_dark),
+        error = painterResource(R.drawable.song_dark),
+    )
 }
