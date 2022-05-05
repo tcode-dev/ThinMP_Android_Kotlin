a45ed2485752588860f5f87d80cffe8eb286abce
tcode <tk@tcode.dev>
2022-05-06 08:18:43 +0900

    show ArtistDetailScreen

--- app/src/main/java/dev/tcode/thinmp/view/screen/ArtistDetailScreen.kt
+++ app/src/main/java/dev/tcode/thinmp/view/screen/ArtistDetailScreen.kt
@@ -1,9 +1,28 @@
 package dev.tcode.thinmp.view.screen
 
 import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.zIndex
+import androidx.constraintlayout.compose.ConstraintLayout
+import dev.tcode.thinmp.view.divider.DividerView
+import dev.tcode.thinmp.view.image.ImageView
+import dev.tcode.thinmp.view.row.MediaRowView
+import dev.tcode.thinmp.view.topbar.HeroTopbarView
 import dev.tcode.thinmp.viewModel.ArtistDetailViewModel
 
 @ExperimentalFoundationApi
@@ -17,5 +36,79 @@ fun ArtistDetailScreen(
 ) {
     val uiState = viewModel.uiState
 
-    Text(uiState.primaryText)
+    Box(Modifier.fillMaxWidth()) {
+        val lazyListState = rememberLazyListState()
+
+        Box(Modifier.zIndex(1F)) {
+            HeroTopbarView(
+                uiState.primaryText,
+                index = lazyListState.firstVisibleItemIndex,
+                lazyListState.firstVisibleItemScrollOffset
+            )
+        }
+        LazyColumn(state = lazyListState) {
+            item {
+                ConstraintLayout(
+                    Modifier
+                        .fillMaxWidth()
+                        .height(LocalConfiguration.current.screenWidthDp.dp)
+                ) {
+                    val (primary, secondary, tertiary) = createRefs()
+                    ImageView(
+                        uri = uiState.imgUri,
+                        contentScale = ContentScale.FillWidth,
+                        modifier = Modifier.fillMaxSize()
+                    )
+                    Box(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .height(200.dp)
+                            .constrainAs(primary) {
+                                top.linkTo(parent.bottom, margin = (-200).dp)
+                            }
+                            .background(
+                                brush = Brush.verticalGradient(
+                                    0.0f to MaterialTheme.colors.surface.copy(alpha = 0F),
+                                    1.0F to MaterialTheme.colors.surface,
+                                )
+                            ),
+                    ) {
+                    }
+                    Row(
+                        Modifier
+                            .fillMaxWidth()
+                            .height(50.dp)
+                            .constrainAs(secondary) {
+                                top.linkTo(parent.bottom, margin = (-90).dp)
+                            },
+                        verticalAlignment = Alignment.CenterVertically,
+                        horizontalArrangement = Arrangement.Center,
+                    ) {
+                        Text(
+                            uiState.primaryText,
+                            fontWeight = FontWeight.Bold,
+                        )
+                    }
+                    Row(
+                        Modifier
+                            .fillMaxWidth()
+                            .height(25.dp)
+                            .constrainAs(tertiary) {
+                                top.linkTo(parent.bottom, margin = (-55).dp)
+                            },
+                        verticalAlignment = Alignment.CenterVertically,
+                        horizontalArrangement = Arrangement.Center,
+                    ) {
+                        Text(
+                            uiState.secondaryText,
+                        )
+                    }
+                }
+            }
+            items(uiState.songs) { song ->
+                MediaRowView(song.name, song.artistName, song.getUri())
+                DividerView()
+            }
+        }
+    }
 }
