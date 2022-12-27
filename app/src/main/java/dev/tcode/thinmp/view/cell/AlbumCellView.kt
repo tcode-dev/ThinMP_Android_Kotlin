24a0fdaf9c1373f5885d0e42921228f533149817
tcode <tk@tcode.dev>
2022-12-27 17:38:47 +0900

    radius image

--- app/src/main/java/dev/tcode/thinmp/view/cell/AlbumCellView.kt
+++ app/src/main/java/dev/tcode/thinmp/view/cell/AlbumCellView.kt
@@ -3,9 +3,12 @@ package dev.tcode.thinmp.view.cell
 import android.net.Uri
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextOverflow
@@ -31,9 +34,11 @@ fun AlbumCellView(
             }) {
             Box {
                 ImageView(
-                    uri = uri, modifier = Modifier
+                    uri = uri,
+                    modifier = Modifier
                         .width(size)
                         .height(size)
+                        .clip(RoundedCornerShape(4.dp))
                 )
             }
             Text(
--- app/src/main/java/dev/tcode/thinmp/view/row/MediaRowView.kt
+++ app/src/main/java/dev/tcode/thinmp/view/row/MediaRowView.kt
@@ -5,9 +5,11 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -18,7 +20,7 @@ import dev.tcode.thinmp.view.image.ImageView
 fun MediaRowView(primaryText: String, secondaryText: String, uri: Uri) {
     Column(modifier = Modifier.padding(start = 20.dp)) {
         Row(modifier = Modifier.padding(top = 5.dp, bottom = 5.dp)) {
-            ImageView(uri = uri, modifier = Modifier.size(44.dp))
+            ImageView(uri = uri, modifier = Modifier.size(44.dp).clip(RoundedCornerShape(4.dp)))
             Column(modifier = Modifier.padding(start = 8.dp)) {
                 Text(primaryText, fontSize = 14.sp, maxLines = 1, overflow = TextOverflow.Ellipsis)
                 Text(secondaryText, fontSize = 12.sp, maxLines = 1, overflow = TextOverflow.Ellipsis)
