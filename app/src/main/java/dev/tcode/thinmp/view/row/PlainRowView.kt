dd0c502e103ace95797a01b8047c6613d92b6716
tcode <tk@tcode.dev>
2022-12-28 16:55:20 +0900

    add DividerView in PlainRowView

--- app/src/main/java/dev/tcode/thinmp/view/row/PlainRowView.kt
+++ app/src/main/java/dev/tcode/thinmp/view/row/PlainRowView.kt
@@ -1,5 +1,6 @@
 package dev.tcode.thinmp.view.row
 
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Text
@@ -7,10 +8,14 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
+import dev.tcode.thinmp.view.divider.DividerView
 
 @Composable
 fun PlainRowView(primaryText: String, modifier: Modifier = Modifier) {
-    Row(modifier = modifier.padding(10.dp)) {
-        Text(primaryText, maxLines = 1, overflow = TextOverflow.Ellipsis)
+    Column(modifier = Modifier.padding(start = 20.dp)) {
+        Row(modifier = modifier.padding(10.dp)) {
+            Text(primaryText, maxLines = 1, overflow = TextOverflow.Ellipsis)
+        }
+        DividerView()
     }
 }
