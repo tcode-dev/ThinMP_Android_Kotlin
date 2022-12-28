0e918df5f2c319f67c9975dc043a21c1328f14bb
tcode <tk@tcode.dev>
2022-12-28 16:32:23 +0900

    add ListTopbarView in ArtistsScreen

--- app/src/main/java/dev/tcode/thinmp/view/screen/ArtistsScreen.kt
+++ app/src/main/java/dev/tcode/thinmp/view/screen/ArtistsScreen.kt
@@ -2,17 +2,19 @@ package dev.tcode.thinmp.view.screen
 
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.unit.dp
+import androidx.compose.ui.zIndex
 import androidx.navigation.NavHostController
 import dev.tcode.thinmp.view.row.PlainRowView
+import dev.tcode.thinmp.view.topbar.ListTopbarView
 import dev.tcode.thinmp.viewModel.ArtistsViewModel
 
 @ExperimentalFoundationApi
@@ -23,9 +25,16 @@ fun ArtistsScreen(
 ) {
     val uiState = viewModel.uiState
 
-    Column {
-        Text(text = "Artists")
-        LazyColumn {
+    Box {
+        val lazyListState = rememberLazyListState()
+
+        Box(Modifier.zIndex(3F)) {
+            ListTopbarView(navController, "Artists", lazyListState.firstVisibleItemScrollOffset)
+        }
+        LazyColumn(state = lazyListState) {
+            item {
+                SpacerView()
+            }
             items(uiState.artists) { artist ->
                 PlainRowView(artist.name, modifier = Modifier
                     .clickable {
