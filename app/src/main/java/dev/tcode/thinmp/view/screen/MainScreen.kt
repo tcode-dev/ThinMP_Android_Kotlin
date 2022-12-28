56326c6e889906f3e78a255f5aff56edb91dd512
tcode <tk@tcode.dev>
2022-12-28 17:07:04 +0900

    design MainScreen

--- app/src/main/java/dev/tcode/thinmp/view/screen/MainScreen.kt
+++ app/src/main/java/dev/tcode/thinmp/view/screen/MainScreen.kt
@@ -1,10 +1,18 @@
 package dev.tcode.thinmp.view.screen
 
-import androidx.compose.foundation.layout.Column
-import androidx.compose.material.Button
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
+import dev.tcode.thinmp.view.row.PlainRowView
 import dev.tcode.thinmp.viewModel.MainViewModel
 
 @Composable
@@ -12,11 +20,36 @@ fun MainScreen(navController: NavController, viewModel: MainViewModel = MainView
     val uiState = viewModel.uiState
 
     Column {
-        Text("Main Screen!!")
-        uiState.menuList.forEach { menu ->
-            Button(onClick = { navController.navigate(menu.key) }) {
-                Text(text = menu.label)
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .statusBarsPadding()
+                .height(50.dp)
+                .padding(start = 20.dp),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+            Text(
+                "Library",
+                textAlign = TextAlign.Left,
+                maxLines = 1,
+                overflow = TextOverflow.Ellipsis,
+                fontWeight = FontWeight.Bold,
+                fontSize = 24.sp
+            )
+            Box(
+                contentAlignment = Alignment.Center,
+                modifier = Modifier
+                    .width(50.dp)
+                    .height(50.dp)
+                    .clickable { }) {
             }
+        }
+        uiState.menuList.forEach { menu ->
+            PlainRowView(menu.label, modifier = Modifier
+                .clickable {
+                    navController.navigate(menu.key)
+                })
         }
     }
 }
