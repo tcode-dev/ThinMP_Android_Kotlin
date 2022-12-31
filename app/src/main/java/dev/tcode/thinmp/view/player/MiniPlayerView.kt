6d61a7083704ae3131bc6259dc86506c4d1fad59
tcode <tk@tcode.dev>
2022-12-31 13:23:40 +0900

    change uiState toMutableStateFlow in MiniPlayerViewModel

--- app/src/main/java/dev/tcode/thinmp/view/player/MiniPlayerView.kt
+++ app/src/main/java/dev/tcode/thinmp/view/player/MiniPlayerView.kt
@@ -1,24 +1,27 @@
 package dev.tcode.thinmp.view.player
 
 import android.util.Log
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.lifecycle.viewmodel.compose.viewModel
 import dev.tcode.thinmp.view.image.ImageView
 import dev.tcode.thinmp.viewModel.MiniPlayerViewModel
 
 @Composable
-fun MiniPlayerView(viewModel: MiniPlayerViewModel = MiniPlayerViewModel(LocalContext.current)) {
-    val uiState = viewModel.uiState
+fun MiniPlayerView(viewModel: MiniPlayerViewModel = viewModel()) {
+    val uiState by viewModel.uiState.collectAsState()
 
 //    if (!uiState.isVisible) {
 //        Log.d("MiniPlayerView", "false")
@@ -31,6 +34,10 @@ fun MiniPlayerView(viewModel: MiniPlayerViewModel = MiniPlayerViewModel(LocalCon
             modifier = Modifier
                 .size(44.dp)
                 .clip(RoundedCornerShape(4.dp))
+                .clickable {
+                    Log.d("MiniPlayerView", "clicked")
+                    viewModel.forceUpdate()
+                }
         )
         Text(
             uiState.primaryText,
--- app/src/main/java/dev/tcode/thinmp/viewModel/MiniPlayerViewModel.kt
+++ app/src/main/java/dev/tcode/thinmp/viewModel/MiniPlayerViewModel.kt
@@ -7,8 +7,13 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewmodel.compose.viewModel
 import dev.tcode.thinmp.player.MusicPlayer
 import dev.tcode.thinmp.player.MusicPlayerListener
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
 
 data class MiniPlayerUiState(
     var primaryText: String = "",
@@ -16,30 +21,51 @@ data class MiniPlayerUiState(
     var isVisible: Boolean = false
 )
 
-class MiniPlayerViewModel(context: Context) : ViewModel(), MusicPlayerListener {
-    private var musicPlayer: MusicPlayer
-    var uiState by mutableStateOf(MiniPlayerUiState())
-        private set
+class MiniPlayerViewModel( private val context: Context) : ViewModel(), MusicPlayerListener {
+    private val _uiState = MutableStateFlow(MiniPlayerUiState())
+    val uiState: StateFlow<MiniPlayerUiState> = _uiState.asStateFlow()
+//    var uiState by mutableStateOf(MiniPlayerUiState())
+//        private set
+//    private var musicPlayer: MusicPlayer
 
     init {
-        musicPlayer = MusicPlayer(context)
-        musicPlayer.setListener(this)
+//        musicPlayer = MusicPlayer(context)
+//        musicPlayer.setListener(this)
     }
 
     fun update() {
-        val song = musicPlayer.getCurrentSong()
-        if (song != null) {
-            Log.d("MiniPlayerViewModel", "true")
-            Log.d("MiniPlayerViewModel", song.name)
-            uiState.primaryText = song.name
-            uiState.imageUri = song.getImageUri()
-            uiState.isVisible = true
-        } else {
-            Log.d("MiniPlayerViewModel", "false")
-            uiState.primaryText = ""
-            uiState.imageUri = Uri.EMPTY
-            uiState.isVisible = false
-        }
+//        val song = musicPlayer.getCurrentSong()
+//        if (song != null) {
+//            Log.d("MiniPlayerViewModel", "true")
+//            Log.d("MiniPlayerViewModel", song.name)
+//            _uiState.update { currentState ->
+//                currentState.copy(
+//                    primaryText = song.name,
+//                    imageUri = song.getImageUri(),
+//                    isVisible = true
+//                )
+//            }
+//        } else {
+//            Log.d("MiniPlayerViewModel", "false")
+//            _uiState.update { currentState ->
+//                currentState.copy(
+//                    primaryText = "",
+//                    imageUri = Uri.EMPTY,
+//                    isVisible = false
+//                )
+//            }
+//        }
+    }
+
+    fun forceUpdate() {
+        Log.d("MiniPlayerViewModel", "forceUpdate")
+            _uiState.update { currentState ->
+                currentState.copy(
+                    primaryText = "testtest",
+                    imageUri = Uri.EMPTY,
+                    isVisible = true
+                )
+            }
     }
 
     override fun onBind() {
