24ad515db013cb900ff1437ab3a04c7faf152aaf
tcode <tk@tcode.dev>
2023-01-04 09:38:45 +0900

    refactor

--- app/src/main/java/dev/tcode/thinmp/view/topbar/ListTopbarView.kt
+++ app/src/main/java/dev/tcode/thinmp/view/topbar/ListTopbarView.kt
@@ -15,7 +15,6 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
 import dev.tcode.thinmp.R
 
