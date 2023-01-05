e94f9263d8646a5608ad0dc0401106d68b7d7e9b
tcode <tk@tcode.dev>
2023-01-05 15:40:41 +0900

    add MiniPlayerView in ArtistsScreen

--- app/src/main/java/dev/tcode/thinmp/view/screen/ArtistsScreen.kt
+++ app/src/main/java/dev/tcode/thinmp/view/screen/ArtistsScreen.kt
@@ -2,17 +2,24 @@ package dev.tcode.thinmp.view.screen
 
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.zIndex
+import androidx.constraintlayout.compose.ConstraintLayout
+import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavHostController
+import dev.tcode.thinmp.constant.StyleConstant
+import dev.tcode.thinmp.view.player.MiniPlayerView
 import dev.tcode.thinmp.view.row.PlainRowView
 import dev.tcode.thinmp.view.topbar.ListTopbarView
+import dev.tcode.thinmp.view.util.EmptyMiniPlayerView
 import dev.tcode.thinmp.view.util.EmptyTopbarView
 import dev.tcode.thinmp.viewModel.ArtistsViewModel
 
@@ -20,12 +27,15 @@ import dev.tcode.thinmp.viewModel.ArtistsViewModel
 @Composable
 fun ArtistsScreen(
     navController: NavHostController,
-    viewModel: ArtistsViewModel = ArtistsViewModel(LocalContext.current)
+    viewModel: ArtistsViewModel = viewModel()
 ) {
-    val uiState = viewModel.uiState
+    val uiState by viewModel.uiState.collectAsState()
 
-    Box {
+    ConstraintLayout(Modifier.fillMaxSize()) {
+        val (miniPlayer) = createRefs()
         val lazyListState = rememberLazyListState()
+        val miniPlayerHeight =
+            WindowInsets.navigationBars.asPaddingValues().calculateBottomPadding().value + StyleConstant.ROW_HEIGHT
 
         Box(Modifier.zIndex(3F)) {
             ListTopbarView(navController, "Artists", lazyListState.firstVisibleItemScrollOffset)
@@ -40,6 +50,15 @@ fun ArtistsScreen(
                         navController.navigate("artistDetail/${artist.id}")
                     })
             }
+            item {
+                EmptyMiniPlayerView()
+            }
+        }
+        Box(modifier = Modifier
+            .constrainAs(miniPlayer) {
+                top.linkTo(parent.bottom, margin = (-miniPlayerHeight).dp)
+            }) {
+            MiniPlayerView()
         }
     }
 }
--- app/src/main/java/dev/tcode/thinmp/viewModel/ArtistsViewModel.kt
+++ app/src/main/java/dev/tcode/thinmp/viewModel/ArtistsViewModel.kt
@@ -1,28 +1,35 @@
 package dev.tcode.thinmp.viewModel
 
+import android.app.Application
 import android.content.Context
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.setValue
-import androidx.lifecycle.ViewModel
+import androidx.lifecycle.AndroidViewModel
 import dev.tcode.thinmp.model.media.ArtistModel
 import dev.tcode.thinmp.service.ArtistsService
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
 
 data class ArtistsUiState(
     var artists: List<ArtistModel> = emptyList()
 )
 
-class ArtistsViewModel(context: Context) : ViewModel() {
-    var uiState by mutableStateOf(ArtistsUiState())
-        private set
+class ArtistsViewModel(application: Application) : AndroidViewModel(application) {
+    private val _uiState = MutableStateFlow(ArtistsUiState())
+    val uiState: StateFlow<ArtistsUiState> = _uiState.asStateFlow()
 
     init {
-        load(context)
+        load(application)
     }
 
-    fun load(context: Context) {
+    private fun load(context: Context) {
         val service = ArtistsService(context)
+        val artists = service.findAll()
 
-        uiState.artists = service.findAll()
+        _uiState.update { currentState ->
+            currentState.copy(
+                artists = artists
+            )
+        }
     }
 }
