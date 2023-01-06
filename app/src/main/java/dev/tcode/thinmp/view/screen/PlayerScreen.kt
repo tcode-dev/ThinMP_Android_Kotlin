5beeb15248c9635b71ba25a5bc15789ae5ca1434
tcode <tk@tcode.dev>
2023-01-06 14:47:19 +0900

    add buttons in PlayerScreen

--- app/src/main/java/dev/tcode/thinmp/view/screen/PlayerScreen.kt
+++ app/src/main/java/dev/tcode/thinmp/view/screen/PlayerScreen.kt
@@ -2,13 +2,14 @@ package dev.tcode.thinmp.view.screen
 
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Icon
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Slider
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.getValue
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.blur
@@ -16,12 +17,15 @@ import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalConfiguration
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.constraintlayout.compose.ConstraintLayout
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
+import com.google.accompanist.flowlayout.MainAxisAlignment
+import dev.tcode.thinmp.R
 import dev.tcode.thinmp.constant.StyleConstant
 import dev.tcode.thinmp.view.image.ImageView
 import dev.tcode.thinmp.view.util.CustomLifecycleEventObserver
@@ -30,14 +34,13 @@ import dev.tcode.thinmp.viewModel.PlayerViewModel
 @ExperimentalFoundationApi
 @Composable
 fun PlayerScreen(
-    navController: NavController,
-    viewModel: PlayerViewModel = viewModel()
+    navController: NavController, viewModel: PlayerViewModel = viewModel()
 ) {
     val uiState by viewModel.uiState.collectAsState()
 
     CustomLifecycleEventObserver(viewModel)
 
-    ConstraintLayout(Modifier.fillMaxSize()) {
+    Column(Modifier.fillMaxSize()) {
         val imageSize: Dp = LocalConfiguration.current.screenWidthDp.dp / 2
 
         ConstraintLayout(
@@ -45,15 +48,12 @@ fun PlayerScreen(
                 .fillMaxWidth()
                 .height(LocalConfiguration.current.screenWidthDp.dp)
         ) {
-            val (primary, secondary, tertiary) = createRefs()
+            val (primary, secondary, tertiary, utilButton) = createRefs()
 
             ImageView(
-                uri = uiState.imageUri,
-                contentScale = ContentScale.FillWidth,
-                modifier = Modifier
+                uri = uiState.imageUri, contentScale = ContentScale.FillWidth, modifier = Modifier
                     .fillMaxSize()
-                    .blur(20.dp),
-                painter = null
+                    .blur(20.dp), painter = null
             )
             Box(
                 modifier = Modifier
@@ -68,16 +68,12 @@ fun PlayerScreen(
                             1.0F to MaterialTheme.colors.surface,
                         )
                     ),
-            ) {
-            }
+            ) {}
             Box(
-                contentAlignment = Alignment.Center,
-                modifier = Modifier.fillMaxSize()
+                contentAlignment = Alignment.Center, modifier = Modifier.fillMaxSize()
             ) {
                 ImageView(
-                    uri = uiState.imageUri,
-                    contentScale = ContentScale.FillWidth,
-                    modifier = Modifier
+                    uri = uiState.imageUri, contentScale = ContentScale.FillWidth, modifier = Modifier
                         .size(imageSize)
                         .clip(RoundedCornerShape(StyleConstant.IMAGE_CORNER_SIZE.dp))
                 )
@@ -92,7 +88,6 @@ fun PlayerScreen(
                 verticalAlignment = Alignment.CenterVertically,
                 horizontalArrangement = Arrangement.Center,
             ) {
-
                 Text(
                     uiState.primaryText,
                     fontWeight = FontWeight.Bold,
@@ -111,6 +106,76 @@ fun PlayerScreen(
                 Text(
                     uiState.secondaryText,
                 )
+            }
+        }
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(LocalConfiguration.current.screenWidthDp.dp),
+            verticalArrangement = Arrangement.SpaceEvenly
+        ) {
+            var sliderPosition by remember { mutableStateOf(0f) }
+            Slider(value = sliderPosition, onValueChange = { sliderPosition = it }, modifier = Modifier.padding(start = 30.dp, end = 30.dp))
+            Row(
+                modifier = Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.SpaceEvenly, verticalAlignment = Alignment.CenterVertically
+            ) {
+                Box(contentAlignment = Alignment.Center, modifier = Modifier
+                    .clickable { viewModel.next() }) {
+                    Icon(
+                        painter = painterResource(id = R.drawable.round_skip_previous_24), contentDescription = null, modifier = Modifier.size(72.dp)
+                    )
+                }
+                Box(contentAlignment = Alignment.Center, modifier = Modifier
+                    .clickable { viewModel.next() }) {
+                    Icon(
+                        painter = painterResource(id = R.drawable.round_play_arrow_24), contentDescription = null, modifier = Modifier.size(88.dp)
+                    )
+                }
+                Box(contentAlignment = Alignment.Center, modifier = Modifier
+                    .clickable { viewModel.next() }) {
+                    Icon(
+                        painter = painterResource(id = R.drawable.round_skip_next_24), contentDescription = null, modifier = Modifier.size(72.dp)
+                    )
+                }
+            }
+            Row(
+                modifier = Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.SpaceEvenly
+            ) {
+                Box(contentAlignment = Alignment.Center, modifier = Modifier
+                    .size(StyleConstant.BUTTON_SIZE.dp)
+                    .clickable { viewModel.next() }) {
+                    Icon(
+                        painter = painterResource(id = R.drawable.round_repeat_24), contentDescription = null, modifier = Modifier.size(StyleConstant.IMAGE_SIZE.dp)
+                    )
+                }
+                Box(contentAlignment = Alignment.Center, modifier = Modifier
+                    .size(StyleConstant.BUTTON_SIZE.dp)
+                    .clickable { viewModel.next() }) {
+                    Icon(
+                        painter = painterResource(id = R.drawable.round_shuffle_24), contentDescription = null, modifier = Modifier.size(StyleConstant.IMAGE_SIZE.dp)
+                    )
+                }
+                Box(contentAlignment = Alignment.Center, modifier = Modifier
+                    .size(StyleConstant.BUTTON_SIZE.dp)
+                    .clickable { viewModel.next() }) {
+                    Icon(
+                        painter = painterResource(id = R.drawable.round_person_24), contentDescription = null, modifier = Modifier.size(StyleConstant.IMAGE_SIZE.dp)
+                    )
+                }
+                Box(contentAlignment = Alignment.Center, modifier = Modifier
+                    .size(StyleConstant.BUTTON_SIZE.dp)
+                    .clickable { viewModel.next() }) {
+                    Icon(
+                        painter = painterResource(id = R.drawable.round_favorite_24), contentDescription = null, modifier = Modifier.size(StyleConstant.IMAGE_SIZE.dp)
+                    )
+                }
+                Box(contentAlignment = Alignment.Center, modifier = Modifier
+                    .size(StyleConstant.BUTTON_SIZE.dp)
+                    .clickable { viewModel.next() }) {
+                    Icon(
+                        painter = painterResource(id = R.drawable.round_playlist_add_24), contentDescription = null, modifier = Modifier.size(StyleConstant.IMAGE_SIZE.dp)
+                    )
+                }
             }
         }
     }
