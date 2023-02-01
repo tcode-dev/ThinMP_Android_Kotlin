8f0cf83979d5c207b74aff598500fd33fb7f9311
tcode <tk@tcode.dev>
2023-02-02 00:10:59 +0900

    refactor

--- app/src/main/java/dev/tcode/thinmp/service/MainService.kt
+++ app/src/main/java/dev/tcode/thinmp/service/MainService.kt
@@ -3,10 +3,7 @@ package dev.tcode.thinmp.service
 import android.content.Context
 import dev.tcode.thinmp.constant.MainMenuEnum
 import dev.tcode.thinmp.model.media.AlbumModel
-import dev.tcode.thinmp.model.media.ArtistDetailModel
 import dev.tcode.thinmp.repository.media.AlbumRepository
-import dev.tcode.thinmp.repository.media.ArtistRepository
-import dev.tcode.thinmp.repository.media.SongRepository
 
 class MainService(val context: Context) {
     fun getMenu(): List<MainMenuEnum> {
