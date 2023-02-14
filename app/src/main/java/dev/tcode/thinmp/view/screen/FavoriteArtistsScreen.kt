f08356931b8f3c95e87b71185a42dccb6e5736ac
tcode <tk@tcode.dev>
2023-02-15 00:08:47 +0900

    add menu button in FavoriteArtistsScreen

--- app/src/main/java/dev/tcode/thinmp/view/screen/FavoriteArtistsScreen.kt
+++ app/src/main/java/dev/tcode/thinmp/view/screen/FavoriteArtistsScreen.kt
@@ -1,19 +1,24 @@
 package dev.tcode.thinmp.view.screen
 
 import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.gestures.detectTapGestures
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.DropdownMenu
 import androidx.compose.material.DropdownMenuItem
+import androidx.compose.material.Icon
 import androidx.compose.material.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.DpOffset
 import androidx.compose.ui.unit.dp
@@ -26,6 +31,7 @@ import dev.tcode.thinmp.constant.NavConstant
 import dev.tcode.thinmp.constant.StyleConstant
 import dev.tcode.thinmp.view.player.MiniPlayerView
 import dev.tcode.thinmp.view.row.PlainRowView
+import dev.tcode.thinmp.view.topAppBar.MenuTopAppBarView
 import dev.tcode.thinmp.view.topAppBar.PlainTopAppBarView
 import dev.tcode.thinmp.view.util.CustomLifecycleEventObserver
 import dev.tcode.thinmp.view.util.EmptyMiniPlayerView
@@ -48,7 +54,24 @@ fun FavoriteArtistsScreen(
         val (miniPlayer) = createRefs()
 
         Box(Modifier.zIndex(3F)) {
-            PlainTopAppBarView(navController, stringResource(R.string.favorite_artists), lazyListState.firstVisibleItemScrollOffset)
+            MenuTopAppBarView(navController, stringResource(R.string.favorite_artists), lazyListState.firstVisibleItemScrollOffset) {
+                val expanded = remember { mutableStateOf(false) }
+
+                Box(contentAlignment = Alignment.Center, modifier = Modifier
+                    .size(StyleConstant.BUTTON_SIZE.dp)
+                    .clip(RoundedCornerShape(StyleConstant.IMAGE_CORNER_SIZE.dp))
+                    .clickable { expanded.value = !expanded.value }) {
+                    Icon(painter = painterResource(id = R.drawable.round_more_vert_24), contentDescription = null, modifier = Modifier.size(StyleConstant.ICON_SIZE.dp))
+                    DropdownMenu(expanded = expanded.value, offset = DpOffset((-1).dp, 0.dp), onDismissRequest = { expanded.value = false }) {
+                        DropdownMenuItem(onClick = {
+//                            navController.navigate(NavConstant.FAVORITE_SONGS_EDIT)
+                            expanded.value = false
+                        }) {
+                            Text(stringResource(R.string.edit))
+                        }
+                    }
+                }
+            }
         }
         LazyColumn(state = lazyListState) {
             item {
