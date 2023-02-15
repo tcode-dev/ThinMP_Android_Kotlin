852dca1c9832ff6b70a15c4a53b3bc83d52f484a
tcode <tk@tcode.dev>
2023-02-16 00:04:34 +0900

    add EditTopAppBarView

--- app/src/main/java/dev/tcode/thinmp/view/screen/FavoriteSongsEditScreen.kt
+++ app/src/main/java/dev/tcode/thinmp/view/screen/FavoriteSongsEditScreen.kt
@@ -1,23 +1,29 @@
 package dev.tcode.thinmp.view.screen
 
 import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.gestures.detectDragGesturesAfterLongPress
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.zIndex
 import androidx.constraintlayout.compose.ConstraintLayout
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
 import dev.tcode.thinmp.R
+import dev.tcode.thinmp.constant.StyleConstant
 import dev.tcode.thinmp.view.row.MediaRowView
-import dev.tcode.thinmp.view.topAppBar.PlainTopAppBarView
+import dev.tcode.thinmp.view.topAppBar.EditTopAppBarView
 import dev.tcode.thinmp.view.util.CustomLifecycleEventObserver
 import dev.tcode.thinmp.view.util.EmptyTopbarView
 import dev.tcode.thinmp.viewModel.FavoriteSongsViewModel
@@ -34,7 +40,13 @@ fun FavoriteSongsEditScreen(
 
     ConstraintLayout(Modifier.fillMaxSize()) {
         Box(Modifier.zIndex(3F)) {
-            PlainTopAppBarView(navController, stringResource(R.string.edit), lazyListState.firstVisibleItemScrollOffset)
+            EditTopAppBarView(navController, stringResource(R.string.edit), lazyListState.firstVisibleItemScrollOffset) {
+                Box(contentAlignment = Alignment.Center, modifier = Modifier
+                    .clip(RoundedCornerShape(StyleConstant.IMAGE_CORNER_SIZE.dp))
+                    .clickable { }) {
+                    Text(stringResource(R.string.done))
+                }
+            }
         }
         LazyColumn(
                 modifier = Modifier
---
+++ app/src/main/java/dev/tcode/thinmp/view/topAppBar/EditTopAppBarView.kt
@@ -0,0 +1,57 @@
+package dev.tcode.thinmp.view.topAppBar
+
+import androidx.compose.animation.*
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+import dev.tcode.thinmp.R
+import dev.tcode.thinmp.constant.StyleConstant
+
+@Composable
+fun EditTopAppBarView(navController: NavController, title: String, offset: Int, content: @Composable BoxScope.() -> Unit) {
+    Box {
+        AnimatedVisibility(
+            visible = offset > 1, enter = fadeIn(initialAlpha = 0.3F), exit = fadeOut(targetAlpha = 0.3F)
+        ) {
+            Box(
+                modifier = Modifier
+                    .background(color = MaterialTheme.colors.secondary)
+                    .fillMaxWidth()
+                    .statusBarsPadding()
+                    .height(StyleConstant.ROW_HEIGHT.dp)
+            ) {}
+        }
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .statusBarsPadding()
+                .height(StyleConstant.ROW_HEIGHT.dp)
+                .padding(start = StyleConstant.PADDING_LARGE.dp, end = StyleConstant.PADDING_LARGE.dp)
+        ) {
+            Box(contentAlignment = Alignment.Center,
+                modifier = Modifier
+                    .align(alignment = Alignment.CenterStart)
+                    .clip(RoundedCornerShape(StyleConstant.IMAGE_CORNER_SIZE.dp))
+                    .clickable { navController.popBackStack() }) {
+                Text(stringResource(R.string.cancel))
+            }
+            Text(
+                title, textAlign = TextAlign.Center, maxLines = 1, overflow = TextOverflow.Ellipsis, fontWeight = FontWeight.Bold, modifier = Modifier.align(alignment = Alignment.Center)
+            )
+            Box(content = content, modifier = Modifier.align(alignment = Alignment.CenterEnd))
+        }
+    }
+}
