84fe27f0f7a7e6fba28886b6c43d020218be1302
tcode <tk@tcode.dev>
2023-02-25 10:53:40 +0900

    add Shortcut in ArtistDetailScreen

--- app/src/main/java/dev/tcode/thinmp/view/screen/ArtistDetailScreen.kt
+++ app/src/main/java/dev/tcode/thinmp/view/screen/ArtistDetailScreen.kt
@@ -3,25 +3,27 @@ package dev.tcode.thinmp.view.screen
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.gestures.detectTapGestures
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.grid.*
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.DropdownMenu
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.getValue
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.blur
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.DpOffset
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.compose.ui.zIndex
@@ -29,11 +31,11 @@ import androidx.constraintlayout.compose.ConstraintLayout
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
 import dev.tcode.thinmp.R
-import dev.tcode.thinmp.constant.NavConstant
 import dev.tcode.thinmp.constant.StyleConstant
 import dev.tcode.thinmp.model.media.SongModel
 import dev.tcode.thinmp.view.cell.AlbumCellView
 import dev.tcode.thinmp.view.cell.GridCellView
+import dev.tcode.thinmp.view.dropdownMenu.DropdownShortcutView
 import dev.tcode.thinmp.view.image.ImageView
 import dev.tcode.thinmp.view.player.MiniPlayerView
 import dev.tcode.thinmp.view.row.MediaRowView
@@ -54,6 +56,7 @@ fun ArtistDetailScreen(
     val visibleHeroTopbarView =
         lazyGridState.firstVisibleItemIndex > 0 || (lazyGridState.firstVisibleItemScrollOffset / LocalContext.current.resources.displayMetrics.density) > (LocalConfiguration.current.screenWidthDp - (WindowInsets.systemBars.asPaddingValues()
             .calculateTopPadding().value + 90))
+    val itemSize: Dp = LocalConfiguration.current.screenWidthDp.dp / StyleConstant.GRID_MAX_SPAN_COUNT
 
     CustomLifecycleEventObserver(viewModel)
 
@@ -145,12 +148,22 @@ fun ArtistDetailScreen(
                 )
             }
             itemsIndexed(items = uiState.albums) { index, album ->
-                val itemSize: Dp = LocalConfiguration.current.screenWidthDp.dp / StyleConstant.GRID_MAX_SPAN_COUNT
+                Box(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .wrapContentSize(Alignment.TopStart)
+                ) {
+                    val expanded = remember { mutableStateOf(false) }
+                    val close = { expanded.value = false }
 
-                GridCellView(index, StyleConstant.GRID_MAX_SPAN_COUNT, itemSize) {
-                    AlbumCellView(album.name, album.artistName, album.getImageUri(), Modifier.clickable {
-                        navController.navigate("${NavConstant.ALBUM_DETAIL}/${album.id}")
-                    })
+                    GridCellView(index, StyleConstant.GRID_MAX_SPAN_COUNT, itemSize) {
+                        AlbumCellView(album.name, album.artistName, album.getImageUri(), Modifier.pointerInput(Unit) {
+                            detectTapGestures(onLongPress = { expanded.value = true }, onTap = { navController.navigate("${dev.tcode.thinmp.constant.NavConstant.ALBUM_DETAIL}/${album.id}") })
+                        })
+                    }
+                    DropdownMenu(expanded = expanded.value, offset = DpOffset((-1).dp, 0.dp), onDismissRequest = { expanded.value = false }) {
+                        DropdownShortcutView(album.albumId, R.string.add_shortcut, R.string.remove_shortcut, close)
+                    }
                 }
             }
             item(span = { GridItemSpan(StyleConstant.GRID_MAX_SPAN_COUNT) }) {
