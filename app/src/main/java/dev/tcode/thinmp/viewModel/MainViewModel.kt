04210e9dfbe05c40330d881565e56efc12137740
tcode <tk@tcode.dev>
2023-03-30 00:27:39 +0900

    remove asStateFlow

--- app/src/main/java/dev/tcode/thinmp/viewModel/MainViewModel.kt
+++ app/src/main/java/dev/tcode/thinmp/viewModel/MainViewModel.kt
@@ -20,7 +20,7 @@ data class MainUiState(
 class MainViewModel(application: Application) : AndroidViewModel(application), CustomLifecycleEventObserverListener {
     private var initialized: Boolean = false
     private val _uiState = MutableStateFlow(MainUiState())
-    val uiState: StateFlow<MainUiState> = _uiState.asStateFlow()
+    val uiState: StateFlow<MainUiState> = _uiState
 
     init {
         load(application)
