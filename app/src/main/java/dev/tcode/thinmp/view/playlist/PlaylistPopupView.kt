0de75a924c3d1ddf383b30e77ad9cabe3c9b6810
tcode <tk@tcode.dev>
2023-04-08 21:55:17 +0900

    change material2 to material3 of OutlinedTextField

--- app/src/main/java/dev/tcode/thinmp/view/playlist/PlaylistPopupView.kt
+++ app/src/main/java/dev/tcode/thinmp/view/playlist/PlaylistPopupView.kt
@@ -3,10 +3,7 @@ package dev.tcode.thinmp.view.playlist
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.OutlinedTextField
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.OutlinedButton
-import androidx.compose.material3.Text
+import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -23,6 +20,7 @@ import dev.tcode.thinmp.view.row.PlainRowView
 import dev.tcode.thinmp.view.util.CustomLifecycleEventObserver
 import dev.tcode.thinmp.viewModel.PlaylistsViewModel
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun PlaylistPopupView(songId: SongId, visiblePopup: MutableState<Boolean>, viewModel: PlaylistsViewModel = viewModel()) {
     val uiState by viewModel.uiState.collectAsState()
