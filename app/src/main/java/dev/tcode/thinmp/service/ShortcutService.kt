02107eef00a57c15d2a59b0b9b41734d1a911b77
tcode <tk@tcode.dev>
2023-04-15 19:41:43 +0900

    map ShortcutModel

--- app/src/main/java/dev/tcode/thinmp/service/ShortcutService.kt
+++ app/src/main/java/dev/tcode/thinmp/service/ShortcutService.kt
@@ -27,15 +27,13 @@ class ShortcutService(
     fun findAll(): List<ShortcutModel> {
         val shortcutRealmModels = shortcutRepository.findAll()
         val group = shortcutRealmModels.groupBy { it.type }
-        var shortcutArtists: List<ShortcutModel> = emptyList()
-        var shortcutAlbums: List<ShortcutModel> = emptyList()
-        var shortcutPlaylists: List<ShortcutModel> = emptyList()
+        val map = mutableMapOf(ItemType.ARTIST.ordinal to emptyList<ShortcutModel>(), ItemType.ALBUM.ordinal to emptyList(), ItemType.PLAYLIST.ordinal to emptyList())
 
         if (group.containsKey(ItemType.ARTIST.ordinal)) {
             val artistIds = group[ItemType.ARTIST.ordinal]?.map { ArtistId(it.itemId) }!!
             val artists = artistRepository.findByIds(artistIds)
 
-            shortcutArtists = artists.map {
+            map[ItemType.ARTIST.ordinal] = artists.map {
                 val albums = albumRepository.findByArtistId(it.id)
 
                 ShortcutModel(it.artistId, it.name, resources.getString(R.string.artist), albums.first().getImageUri(), ItemType.ARTIST)
@@ -46,7 +44,7 @@ class ShortcutService(
             val albumIds = group[ItemType.ALBUM.ordinal]?.map { AlbumId(it.itemId) }!!
             val albums = albumRepository.findByIds(albumIds)
 
-            shortcutAlbums = albums.map {
+            map[ItemType.ALBUM.ordinal] = albums.map {
                 ShortcutModel(it.albumId, it.name, resources.getString(R.string.album), it.getImageUri(), ItemType.ALBUM)
             }
         }
@@ -55,7 +53,7 @@ class ShortcutService(
             val playlistIds = group[ItemType.PLAYLIST.ordinal]?.map { PlaylistId(it.itemId) }!!
             val playlists = playlistRepository.findByIds(playlistIds)
 
-            shortcutPlaylists = playlists.map {
+            map[ItemType.PLAYLIST.ordinal] = playlists.map {
                 val song = songRepository.findById(it.songs.first().songId)
 
                 ShortcutModel(PlaylistId(it.id), it.name, resources.getString(R.string.playlist), song!!.getImageUri(), ItemType.PLAYLIST)
@@ -63,14 +61,7 @@ class ShortcutService(
         }
 
         val shortcutModels = shortcutRealmModels.mapNotNull { shortcut ->
-            when (shortcut.type) {
-                ItemType.ARTIST.ordinal -> shortcutArtists.firstOrNull { artist -> artist.itemId.toId(artist.type) == shortcut.itemId }
-                ItemType.ALBUM.ordinal -> shortcutAlbums.firstOrNull { album -> album.itemId.toId(album.type) == shortcut.itemId }
-                ItemType.PLAYLIST.ordinal -> shortcutPlaylists.firstOrNull { playlist -> playlist.itemId.toId(playlist.type) == shortcut.itemId }
-                else -> {
-                    throw IllegalArgumentException("Unknown expression")
-                }
-            }
+            map[shortcut.type]?.firstOrNull { item -> item.itemId.toId(item.type) == shortcut.itemId }
         }
 
         if (!validation(shortcutRealmModels, shortcutModels)) {
