f96b747be4c5f1d6c67fbb38c3e7a0b5777db1bb
tcode <tk@tcode.dev>
2023-05-04 12:02:55 +0900

    set color in PermissionView

--- app/src/main/java/dev/tcode/thinmp/view/permission/PermissionView.kt
+++ app/src/main/java/dev/tcode/thinmp/view/permission/PermissionView.kt
@@ -1,18 +1,19 @@
 package dev.tcode.thinmp.view.permission
 
 import androidx.compose.foundation.layout.*
-import androidx.compose.material3.Button
-import androidx.compose.material3.Text
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.SideEffect
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 import com.google.accompanist.permissions.ExperimentalPermissionsApi
 import com.google.accompanist.permissions.PermissionStatus
 import com.google.accompanist.permissions.rememberPermissionState
 import com.google.accompanist.permissions.shouldShowRationale
 import dev.tcode.thinmp.R
+import dev.tcode.thinmp.constant.StyleConstant
 
 @OptIn(ExperimentalPermissionsApi::class)
 @Composable
@@ -31,8 +32,13 @@ fun PermissionView(content: @Composable BoxScope.() -> Unit) {
                     Column(
                         modifier = Modifier.fillMaxSize(), verticalArrangement = Arrangement.Center, horizontalAlignment = Alignment.CenterHorizontally
                     ) {
-                        Text(stringResource(R.string.permission_denied))
-                        Button(onClick = { permissionState.launchPermissionRequest() }) {
+                        Text(
+                            stringResource(R.string.permission_denied),
+                            color = MaterialTheme.colorScheme.primary,
+                            modifier = Modifier.padding(start = StyleConstant.PADDING_LARGE.dp, end = StyleConstant.PADDING_LARGE.dp, bottom = StyleConstant.PADDING_LARGE.dp)
+                        )
+                        Button(colors = ButtonDefaults.buttonColors(contentColor = MaterialTheme.colorScheme.primary, containerColor = MaterialTheme.colorScheme.onSecondary),
+                            onClick = { permissionState.launchPermissionRequest() }) {
                             Text(stringResource(R.string.permission_request))
                         }
                     }
