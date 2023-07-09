0a9ba08d8a3498111b419efdfb28910493d6ba15
tcode <tk@tcode.dev>
2023-07-09 17:00:52 +0900

    DropdownRowView

---
+++ app/src/main/java/dev/tcode/thinmp/view/row/DropdownRowView.kt
@@ -0,0 +1,41 @@
+package dev.tcode.thinmp.view.row
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.gestures.detectTapGestures
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.ColumnScope
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.unit.DpOffset
+import androidx.compose.ui.unit.dp
+import java.util.UUID
+
+@Composable
+fun DropdownRowView(dropdownContent: @Composable ColumnScope.(callback: () -> Unit) -> Unit, content: @Composable ColumnScope.(callback: () -> Unit) -> Unit) {
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .wrapContentSize(Alignment.TopStart)
+    ) {
+        val expanded = remember { mutableStateOf(false) }
+        val callback = { expanded.value = !expanded.value }
+
+        Column(Modifier.pointerInput(UUID.randomUUID()) {
+            detectTapGestures(onLongPress = { callback() }, onTap = { callback() })
+        }) {
+            content(callback = callback)
+            DropdownMenu(expanded = expanded.value, offset = DpOffset((-1).dp, 0.dp), modifier = Modifier.background(MaterialTheme.colorScheme.onBackground), onDismissRequest = callback) {
+                dropdownContent(callback = callback)
+            }
+        }
+    }
+}
--- app/src/main/java/dev/tcode/thinmp/view/screen/FavoriteSongsScreen.kt
+++ app/src/main/java/dev/tcode/thinmp/view/screen/FavoriteSongsScreen.kt
@@ -1,20 +1,15 @@
 package dev.tcode.thinmp.view.screen
 
 import androidx.compose.foundation.ExperimentalFoundationApi
-import androidx.compose.foundation.background
 import androidx.compose.foundation.gestures.detectTapGestures
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.itemsIndexed
-import androidx.compose.material3.DropdownMenu
 import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.Text
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.*
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.unit.DpOffset
 import androidx.compose.ui.unit.dp
 import androidx.constraintlayout.compose.ConstraintLayout
 import androidx.lifecycle.viewmodel.compose.viewModel
@@ -26,12 +21,14 @@ import dev.tcode.thinmp.view.dropdownMenu.PlaylistDropdownMenuItemView
 import dev.tcode.thinmp.view.nav.LocalNavigator
 import dev.tcode.thinmp.view.player.MiniPlayerView
 import dev.tcode.thinmp.view.playlist.PlaylistPopupView
+import dev.tcode.thinmp.view.row.DropdownRowView
 import dev.tcode.thinmp.view.row.MediaRowView
 import dev.tcode.thinmp.view.util.CustomLifecycleEventObserver
 import dev.tcode.thinmp.view.util.EmptyMiniPlayerView
 import dev.tcode.thinmp.view.util.EmptyTopAppBarView
 import dev.tcode.thinmp.view.util.miniPlayerHeight
 import dev.tcode.thinmp.viewModel.FavoriteSongsViewModel
+import java.util.UUID
 
 @ExperimentalFoundationApi
 @Composable
@@ -54,26 +51,18 @@ fun FavoriteSongsScreen(viewModel: FavoriteSongsViewModel = viewModel()) {
                 EmptyTopAppBarView()
             }
             itemsIndexed(uiState.songs) { index, song ->
-                Box(
-                    modifier = Modifier
-                        .fillMaxSize()
-                        .wrapContentSize(Alignment.TopStart)
-                ) {
-                    val expanded = remember { mutableStateOf(false) }
-                    val close = { expanded.value = false }
-                    val closePlaylist = {
+                DropdownRowView(dropdownContent = { callback ->
+                    val callbackPlaylist = {
                         playlistRegisterSongId = song.songId
                         visiblePopup.value = true
-                        close()
+                        callback()
                     }
-
-                    MediaRowView(song.name, song.artistName, song.getImageUri(), Modifier.pointerInput(index) {
-                        detectTapGestures(onLongPress = { expanded.value = true }, onTap = { viewModel.start(index) })
+                    FavoriteSongDropdownMenuItemView(song.songId, { callback() })
+                    PlaylistDropdownMenuItemView(callbackPlaylist)
+                }) {callback ->
+                    MediaRowView(song.name, song.artistName, song.getImageUri(), Modifier.pointerInput(UUID.randomUUID()) {
+                        detectTapGestures(onLongPress = { callback() }, onTap = { viewModel.start(index) })
                     })
-                    DropdownMenu(expanded = expanded.value, offset = DpOffset((-1).dp, 0.dp), modifier = Modifier.background(MaterialTheme.colorScheme.onBackground), onDismissRequest = close) {
-                        FavoriteSongDropdownMenuItemView(song.songId, close)
-                        PlaylistDropdownMenuItemView(closePlaylist)
-                    }
                 }
             }
             item {
