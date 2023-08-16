569417a7c0c33249e3b9bb57cdd3d8ff80b14b85
tcode <tk@tcode.dev>
2023-08-17 08:40:27 +0900

    Update CommonLayoutView.kt

--- app/src/main/java/dev/tcode/thinmp/view/layout/CommonLayoutView.kt
+++ app/src/main/java/dev/tcode/thinmp/view/layout/CommonLayoutView.kt
@@ -1,21 +1,13 @@
 package dev.tcode.thinmp.view.layout
 
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
-import androidx.constraintlayout.compose.ConstraintLayout
 import dev.tcode.thinmp.model.media.valueObject.SongId
-import dev.tcode.thinmp.view.player.MiniPlayerView
 import dev.tcode.thinmp.view.playlist.PlaylistRegisterPopupView
-import dev.tcode.thinmp.view.util.miniPlayerHeight
 
 @Composable
 fun CommonLayoutView(isVisibleMiniPlayer: Boolean = true, content: @Composable ((showPlaylistRegisterPopup: (songId: SongId) -> Unit) -> Unit)) {
-    val miniPlayerHeight = miniPlayerHeight()
     val visiblePopup = remember { mutableStateOf(false) }
     var playlistRegisterSongId = SongId("")
     val togglePopup = { visiblePopup.value = !visiblePopup.value }
@@ -24,21 +16,11 @@ fun CommonLayoutView(isVisibleMiniPlayer: Boolean = true, content: @Composable (
         togglePopup()
     }
 
-    ConstraintLayout(Modifier.fillMaxSize()) {
-        val (miniPlayer) = createRefs()
-
+    MiniPlayerLayoutView(isVisibleMiniPlayer) {
         content(showPlaylistRegisterPopup)
 
         if (visiblePopup.value) {
             PlaylistRegisterPopupView(playlistRegisterSongId, togglePopup)
-        }
-
-        if (isVisibleMiniPlayer) {
-            Box(modifier = Modifier.constrainAs(miniPlayer) {
-                top.linkTo(parent.bottom, margin = (-miniPlayerHeight).dp)
-            }) {
-                MiniPlayerView()
-            }
         }
     }
 }
